ffee73bfe26a6ab2fde82de4538c236deb065066
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-07 04:17:47 -0300

    05-Login user

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const app = express();
+const bcrypt = require('bcrypt');
 
 const mongoUri = `mongodb+srv://francis8976:ieLUQHYjjQOtu3vG@cluster0.xs9gz.mongodb.net/myApp?retryWrites=true&w=majority`;
 mongoose.connect(mongoUri);
@@ -27,6 +28,27 @@ app.post('/api/user',(req,res)=>{
         if(err) res.status(400).send(err);
         res.status(200).send(doc)
     })
+})
+
+app.post('/api/user/login',(req,res)=>{
+    // 1 -find the user,if good, -> move forward
+    User.findOne({'email': req.body.email},(err,user)=>{
+        if(err) res.status(400).send(err);
+        if(!user) res.json({message:'User not found'})
+
+        // 2 - compare the password with the HASHED password on the DB, -> move forward
+        bcrypt.compare(req.body.password,user.password,(err,isMatch)=>{
+            if(err) throw err;
+
+            // 3 - send response
+            res.status(200).send(isMatch)
+        })
+    })
+    
+
+
+
+
 })
 
 
