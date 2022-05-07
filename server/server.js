e7342936dd74ea301c8593c78c2e5025c2872e5a
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-07 03:15:44 -0300

    03-Register user

--- server/server.js
+++ server/server.js
@@ -14,6 +14,20 @@ app.use(bodyParser.json());
 const { User } = require('./models/user');
 
 
+/// Routes
+
+app.post('/api/user',(req,res)=>{
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user.save((err,doc)=>{
+        if(err) res.status(400).send(err);
+        res.status(200).send(doc)
+    })
+})
+
 
 
 const port = process.env.PORT || 3000;
